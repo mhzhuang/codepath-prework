0dfdb48c2c15944ba6c84954ea3b56af540535ce
mhzhuang <zhuangmh@gmail.com>
2022-07-31 01:05:51 -0700

    Finish all the Optional User Stories

--- HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
+++ HelloAndroid/app/src/main/java/com/example/helloandroid/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.helloandroid
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.res.TypedArrayUtils.getText
-import com.google.android.material.textfield.TextInputEditText
 
 // This Kotlin file is where we're going to be handling user interaction
 class MainActivity : AppCompatActivity() {
@@ -19,34 +18,62 @@ class MainActivity : AppCompatActivity() {
         // Show this layout file: activity_main
         setContentView(R.layout.activity_main)
 
-        // Display customized to show 'Hello from {name}'
-        // User can tap a button to change the text color of the label
-        // 1. Add a button to our layout
 
-        // Get a reference to button
-        // 2. Set up logic to know when user has tapped on button
+        // ### Required User Stories
+        // 1. User sees custom text in a label - Hello from {name}!
+        // Complete in file activity_main.xml
+
+        //2. User see's custom background color.
+        // Complete in file activity_main.xml
+
+        // 3. User can tap a button to change the text color of the label.
         findViewById<Button>(R.id.button).setOnClickListener {
-            // handle button tap
-            // 2. Change the color of the text
-            Log.i("Minhui", "Tapped on button")
-            // 1. Get a reference to the text view
-            // 2. Set the color of the text view
+            //Log.i("Minhui", "Tapped on button")
             findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.red))
         }
 
-        // Change string text
-        findViewById<Button>(R.id.button2).setOnClickListener {
-            //EditText simpleEditText = (EditText) findViewById(R.id.et_simple);
-            //String strValue = simpleEditText.getText().toString();
-            findViewById<TextView>(R.id.textView).text =
-                findViewById<EditText>(R.id.et_simple).getText()
-        }
-
-        // Change view color
+        // ### Optional User Stories
+        // 1. User can tap a button to change the color of the background view.
         findViewById<Button>(R.id.button3).setOnClickListener {
             findViewById<View>(R.id.backgroundView).setBackgroundColor(getResources().getColor(R.color.mint))
         }
+
+        // 2. User can tap a button to change the text string of the label - Goodbye 👋.
+        findViewById<Button>(R.id.button4).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text="Goodbye \uD83D\uDC4B"
+        }
+
+        // 3. User can tap on the background view to reset all views to default settings.
+        findViewById<View>(R.id.backgroundView).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text="Hello from Minhui"
+            findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.white))
+            findViewById<View>(R.id.backgroundView).setBackgroundColor(getResources().getColor(R.color.userdefined))
+        }
+
+        // 4. User can update the label text with custom text entered into the text field.
+
+        findViewById<Button>(R.id.button2).setOnClickListener {
+            // a. User can enter text into a text field using the keyboard.
+            var textView = findViewById<EditText>(R.id.et_simple).text
+            // b. User can tap the "Change text string" button to update the label with the text from the text field.
+            if (!textView.toString().equals("")) {
+                findViewById<TextView>(R.id.textView).text = textView.toString()
+            } else {
+                // c. If the text field is empty, update label with default text string.
+                findViewById<TextView>(R.id.textView).text = "Hello CodePath"
+            }
+            // d. The keyboard is dismissed after the button has been tapped.
+            hideKeyboard(this, findViewById(android.R.id.content));
+        }
     }
+
+    // Hide Keyboard Function
+    fun hideKeyboard(context: Context, view: View) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
 }
+
 
 
